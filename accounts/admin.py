8cb36b830e0d0dc0035001fe1ef72f3b717fc5cd
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-12-25 16:17:05 +0300

    add permissions

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,18 +3,30 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-# Register your models here.
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = [
-            "email",
-            "username",
-            "name",
-            "is_staff",
-    ]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+
+    list_display = ("email", "username", "name", "is_staff")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("name", "email")}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "name", "password1", "password2", "is_staff"),
+        }),
+    )
+
+    search_fields = ("username", "email", "name")
+    ordering = ("username",)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
